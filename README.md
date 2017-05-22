5b7666080f536d8e73686641f53f20f01d07e19e
swathi.kudumula@gmail.com <swathi.reddy@infotools.com>
2017-05-22 17:07:06 +1200

    add sample specflow test

---
+++ README.md
@@ -0,0 +1,1 @@
+Spec flow sample project
---
+++ SpecFlowTest/stepDef.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowTest
+{
+   [Binding]
+   public class CalculatorSteps
+   {
+      private int result { get; set; }
+      private Calculator calculator = new Calculator();
+
+      [Given(@"I have entered (.*) into the calculator")]
+      public void GivenIHaveEnteredIntoTheCalculator(int number)
+      {
+         calculator.FirstNumber = number;
+      }
+
+      [Given(@"I have also entered (.*) into the calculator")]
+      public void GivenIHaveAlsoEnteredIntoTheCalculator(int number)
+      {
+         calculator.SecondNumber = number;
+      }
+
+      [When(@"I press add")]
+      public void WhenIPressAdd()
+      {
+         result = calculator.Add();
+      }
+
+      [When(@"I press sub")]
+      public void WhenIPressSub()
+      {
+         result = calculator.Sub();
+      }
+
+
+      [Then(@"the result should be (.*) on the screen")]
+      public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
+      {
+         Assert.AreEqual(expectedResult, result);
+      }
+   }
+}
